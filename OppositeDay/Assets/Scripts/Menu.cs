22b82dbbc3398f6c900686aeb3ceedd34e120dad
Dennis Lieu <dennis.lieu@xazen.de>
2015-04-19 00:42:50 +0200

    - Add menu

---
+++ OppositeDay/Assets/Scripts/Menu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	public void StartGame () 
+	{
+		Application.LoadLevel (1);
+	}
+
+	public void QuitGame ()
+	{
+		Application.Quit ();
+	}
+}
